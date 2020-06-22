6b6e5f73689c724ed8b1a011bcf516ff9be10dc0
vlee13 <vlee13@gmail.com>
2020-06-22 11:28:09 -0700

    edits

--- js/index.js
+++ js/index.js
@@ -1,24 +1,42 @@
-document.getElementById('start-button').onload = (event) => {
-    console.log(event)
+window.onload = (event) => {
+  console.log(event);
 };
 
-const canvas = document.querySelector('#canvas')
-const cxt = canvas.getContext('2d')
-let road = new Image()
-road.src = '../images/spacebg2.jpg'
-let sprite = new Image()
-sprite.src = '../images/zooeysprite.png'
-let yPositionSprite = 0
-let xPositionSprite = 0
-let imageX = 0
-let imageY = 64*2
+const canvas = document.querySelector("#canvas");
+const cxt = canvas.getContext("2d");
+let road = new Image();
+road.src = "../images/spacebg2.jpg";
+let sprite = new Image();
+sprite.src = "../images/zooeysprite.png";
+let yPositionSprite = 0;
+let xPositionSprite = 0;
+let imageX = 0;
+let imageY = 96 * 2;
 let animationID;
-let canvasX = 0
-
+let canvasY2 = 0;
+let canvasY = -canvas.height;
 function animationLoop() {
-    animationID = window.requestAnimationFrame(animationLoop)
-    cxt.clearRect(0, 0, canvas.width, canvas.height)
-    cxt.drawImage(road, 0, 0, canvas.width, canvas.height)
-    cxt.drawImage(sprite, imageX, imageY, 64, 64, xPositionSprite, yPositionSprite, 64, 64) // (imageObj, imageX, imageY, imageWidth, imageHeight, xCanvas, yCanvas, widthCanvas, heightCanvas)
+  cxt.clearRect(0, 50, canvas.width, canvas.height);
+    cxt.drawImage(road, 0, ++canvasY, canvas.width, canvas.height);
+    cxt.drawImage(road, 0, ++canvasY2, canvas.width, canvas.height);
+    if(canvasY >= canvas.height) canvasY = -canvas.height
+    if(canvasY2 >= canvas.height) canvasY2 = -canvas.height
+  cxt.drawImage(
+    sprite,
+    imageX,
+    imageY,
+    96,
+    96,
+    xPositionSprite,
+    yPositionSprite,
+    96,
+    96
+  ); // (imageObj, imageX, imageY, imageWidth, imageHeight, xCanvas, yCanvas, widthCanvas, heightCanvas)
+    animationID = window.requestAnimationFrame(animationLoop);
 }
-animationLoop()
+
+animationLoop();
+
+function moveZooey(e) {
+  console.log(e);
+}
