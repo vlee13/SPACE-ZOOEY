ff5806b44b785998c30866210dfcf6b387eb16fa
vlee13 <vlee13@gmail.com>
2020-06-22 09:58:46 -0700

    .

--- js/index.js
+++ js/index.js
@@ -3,18 +3,22 @@ document.getElementById('start-button').onload = (event) => {
 };
 
 const canvas = document.querySelector('#canvas')
-const ctx = canvas.getContext('2d')
+const cxt = canvas.getContext('2d')
+let road = new Image()
+road.src = '../images/spacebg2.jpg'
 let sprite = new Image()
 sprite.src = '../images/zooeysprite.png'
 let yPositionSprite = 0
 let xPositionSprite = 0
 let imageX = 0
-let imageY = 65*2
+let imageY = 64*2
 let animationID;
 let canvasX = 0
 
 function animationLoop() {
     animationID = window.requestAnimationFrame(animationLoop)
     cxt.clearRect(0, 0, canvas.width, canvas.height)
-    cxt.drawImage(sprite, imageX, imageY, 65, 65, xPositionSprite, yPositionSprite, 65, 65) // (imageObj, imageX, imageY, imageWidth, imageHeight, xCanvas, yCanvas, widthCanvas, heightCanvas)
+    cxt.drawImage(road, 0, 0, canvas.width, canvas.height)
+    cxt.drawImage(sprite, imageX, imageY, 64, 64, xPositionSprite, yPositionSprite, 64, 64) // (imageObj, imageX, imageY, imageWidth, imageHeight, xCanvas, yCanvas, widthCanvas, heightCanvas)
 }
+animationLoop()
